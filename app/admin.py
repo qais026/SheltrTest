97cb55fd140e9a7719171f56f2c33a9818921bff
Qais Feroz <qais026@gmail.com>
2015-01-18 17:39:29 -0500

    Admin UI
    Admin UI now organized into fieldsets

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-
 from app.models import Provider
 # Register your models here.
-admin.site.register(Provider)
+
+class ProviderAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,                              {'fields': ['name']}),
+        ('Administrative Contact',          {'fields': ['admin_firstname2','admin_lastname2']}),
+        ('Details',          {'fields': ['logo', 'URL']}),
+        ('Address',          {'fields': ['street','city','zipcode']}),
+        ('Geocode',          {'fields': ['latitude','longitude']}),
+        ('Characteristics',          {'fields': ['characteristics']}),
+    ]
+
+admin.site.register(Provider, ProviderAdmin)
