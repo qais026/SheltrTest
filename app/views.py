f8d92ad6a0b4375005c733a2a8ac27f1744459e3
Qais Feroz <qais026@gmail.com>
2015-01-22 20:16:42 -0500

    UI files added
    Added vanilla css for Foundation, all files in.

--- app/views.py
+++ app/views.py
@@ -5,3 +5,12 @@ from app.models import Provider
 def index(request):
     providers = Provider.objects.order_by("-name")
     return render(request, 'app/index.html', {"providers": Provider})
+
+# About Us page
+def about(request):
+    return render(request, "about.html")
+
+# Contact Us page
+def contact(request):
+    return render(request, "contact.html")
+
